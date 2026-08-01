c5e751e2aa4f827b2e60172e95fb6bb32d754d34
mmamoneymatrix <mmamoneymatrix@yahoo.com>
2026-08-01 14:24:45 -0400

    Delete MMAMoneyMatrixcodebase directory

--- MMAMoneyMatrixcodebase/backend/routes/get_fighter.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-get_fighter_bp = Blueprint('get_fighter', __name__)
-
-@get_fighter_bp.route('/get-fighter/<int:fighter_id>', methods=['GET'])
-def get_fighter(fighter_id):
-    # Fetch from DB
-    return jsonify({"id": fighter_id, "name": "Sample Fighter", "stats": {}})
